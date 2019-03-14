89c4eb6dc29902edd58dd10e07feba6830b8bc28
Jayaraman <adarsh1999@gmail.com>
2019-03-14 21:35:08 +0530

    Added Libraries

---
+++ app/src/main/java/com/range/electionapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.range.electionapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    /*TODO add google signin
+    get constituency using location
+    */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

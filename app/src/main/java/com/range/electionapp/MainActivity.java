64f9a330454e58cdb44c2594a6a4a51ed3d008ce
Jayaraman <adarsh1999@gmail.com>
2019-03-15 22:25:20 +0530

    Added Files

--- app/src/main/java/com/range/electionapp/MainActivity.java
+++ app/src/main/java/com/range/electionapp/MainActivity.java
@@ -1,9 +1,17 @@
 package com.range.electionapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +19,36 @@ public class MainActivity extends AppCompatActivity {
     /*TODO add google signin
     get constituency using location
     */
+    final private static int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "SIGN IN SUCCESSFULL", Toast.LENGTH_SHORT).show();
+                DatabaseReference df = FirebaseDatabase.getInstance().getReference("Users/" + FirebaseAuth.getInstance().getCurrentUser().getUid());
+                Users u = new Users(FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), FirebaseAuth.getInstance().getCurrentUser().getUid(), FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber(), FirebaseAuth.getInstance().getCurrentUser().getEmail());
+                df.setValue(u);
+
+                Intent i = new Intent(getApplicationContext(), first_Activity.class);
+                startActivity(i);
+                finish();
+            } else
+                Toast.makeText(this, "SIGN IN FAILED", Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        "68de716e1f144b19a68167785444ecce"
+//        "68de716e1f144b19a68167785444ecce"
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .build(),
+                RC_SIGN_IN);
     }
 }

b8b2f2dcf15af43ce8d0a447ea156b6d55fa5ae0
Jayaraman <adarsh1999@gmail.com>
2019-03-16 03:04:28 +0530

    Updated

---
+++ app/src/main/java/com/range/electionapp/VoteridActivity.java
@@ -0,0 +1,13 @@
+package com.range.electionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoteridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voterid);
+    }
+}
